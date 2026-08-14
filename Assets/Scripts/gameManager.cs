7d9e6dfb5904b85ae476ad11a8f619e4f9796324
Khurshed <158274131+rizvi98@users.noreply.github.com>
2026-08-13 22:18:23 -0400

    fixed merge issues

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -70,7 +70,7 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bytesText.text = "Bytes: " + totalBytes.ToString();
+        //bytesText.text = "Bytes: " + totalBytes.ToString();
         if (FindAnyObjectByType<playerInteraction>().shopOpen)
         { 
             menuActive = shopUI;
