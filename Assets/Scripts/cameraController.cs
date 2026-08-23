ca9d141d52beeeec3f69f34add31fbd7c2cbaa5d
virelmarin5 <virelmarin5@gmail.com>
2026-08-24 02:37:52 +0300

    fix

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -21,11 +21,9 @@ public class cameraController : MonoBehaviour
         {
             float mouseX = Input.GetAxisRaw("Mouse X") * sens;
             float mouseY = Input.GetAxisRaw("Mouse Y") * sens;
-
             camRotX -= mouseY;
             camRotX = Mathf.Clamp(camRotX, lockVertMin, lockVertMax);
             transform.localRotation = Quaternion.Euler(camRotX, 0, 0);
-
             transform.parent.Rotate(Vector3.up * mouseX);
         }
     }
