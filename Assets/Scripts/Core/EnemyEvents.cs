19becdac3c23c307e38803bd5b244496bfda0923
Samuel Herman <frydthundor@gmail.com>
2026-09-02 21:19:33 -0400

    rename ShotAtPlayer event to NearMissedPlayer, ready for near miss detection

--- Assets/Scripts/Core/EnemyEvents.cs
+++ Assets/Scripts/Core/EnemyEvents.cs
@@ -5,22 +5,22 @@ public static class EnemyEvents
 {
     public static event Action<EnemyBase> Killed;
 
-    public static event Action<EnemyBase> ShotAtPlayer;
+    public static event Action<EnemyBase> NearMissedPlayer;
 
     public static void RaiseKilled(EnemyBase enemy)
     {
         Killed?.Invoke(enemy);
     }
 
-    public static void RaiseShotAtPlayer(EnemyBase enemy)
+    public static void RaiseNearMissedPlayer(EnemyBase enemy)
     {
-        ShotAtPlayer?.Invoke(enemy);
+        NearMissedPlayer?.Invoke(enemy);
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     private static void clearOldEvents()
     {
         Killed = null;
-        ShotAtPlayer = null;
+        NearMissedPlayer = null;
     }
 }
